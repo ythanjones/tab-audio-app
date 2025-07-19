412fb73248454af5e01f93c60c12705dab5feabe
jonesythan <jonesythan@gmail.com>
2025-07-19 18:27:25 +0000

    made a permanent api key

--- query-service-backend/index.js
+++ query-service-backend/index.js
@@ -1,5 +1,6 @@
 // File: query-service-backend/index.js
 // ChromaDB version - Simplified query service
+require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
